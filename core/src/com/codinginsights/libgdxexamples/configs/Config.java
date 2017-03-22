49fd1327ac44c35b425fa0dfe7a420d849710613
elefher <eldelielev@gmail.com>
2017-03-22 23:12:50 +0200

    a helper class config

---
+++ core/src/com/codinginsights/libgdxexamples/configs/Config.java
@@ -0,0 +1,13 @@
+package com.codinginsights.libgdxexamples.configs;
+
+/**
+ * Created by elefher on 3/22/17.
+ */
+
+public class Config {
+
+  public static final float WORLD_WIDTH = 12.0f;
+  public static final float WORLD_HEIGHT = 20.0f;
+
+  private Config(){}
+}
