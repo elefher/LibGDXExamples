05d1ec788ba087fd5e2887189f507966568dd845
elefher <eldelielev@gmail.com>
2017-03-22 23:11:11 +0200

    change GdxExamples class to extends Game

--- core/src/com/codinginsights/libgdxexamples/GdxExamples.java
+++ core/src/com/codinginsights/libgdxexamples/GdxExamples.java
@@ -1,33 +1,35 @@
 package com.codinginsights.libgdxexamples;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Logger;
+import com.codinginsights.libgdxexamples.assetManager.AssetManagerExample;
 
-public class GdxExamples extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class GdxExamples extends Game {
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+  private SpriteBatch batch;
+
+  @Override
+  public void create() {
+	Gdx.app.setLogLevel(Application.LOG_DEBUG);
+
+	batch = new SpriteBatch();
+
+	setScreen(new AssetManagerExample(this));
+  }
+
+  @Override
+  public void dispose() {
+	batch.dispose();
+  }
+
+  public SpriteBatch getBatch() {
+	return batch;
+  }
 }
